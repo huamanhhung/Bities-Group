587e6a8409e70913c68fdb648ab1b01a8193d2c1
huamanhhung <68018319+huamanhhung@users.noreply.github.com>
2020-07-27 21:41:30 +0700

    Lớp kết nối với cơ sở dữ liệu SQL

--- src/connectionSQL/connectionSQL.java
+++ src/connectionSQL/connectionSQL.java
@@ -1,1 +1,22 @@
+package connectionSQL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class connectionSQL {
+
+    public static Connection ketnoi(String hoaDon) {
+        try {
+            String user = "sa", password = "1", databaseName = "QLIPHONE";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://DESKTOP-TJJ188P\\SQLEXPRESS:1433;databaseName=" + databaseName;
+
+            Connection con = DriverManager.getConnection(url, user, password);
+            return con;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
