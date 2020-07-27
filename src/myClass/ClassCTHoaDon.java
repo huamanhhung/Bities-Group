1f51be28dce557e2b3da814431baa13b1cccc38b
huamanhhung <68018319+huamanhhung@users.noreply.github.com>
2020-07-27 21:48:32 +0700

    Lớp tạo đối tượng hóa đơn và hóa đơn chi tiết

---
+++ src/myClass/ClassCTHoaDon.java
@@ -0,0 +1,63 @@
+package myClass;
+
+import java.io.Serializable;
+
+public class ClassCTHoaDon implements Serializable{
+
+    private String maHD, maSP;
+    private int soLuong;
+    private double donGia, thanhTien;
+
+    public ClassCTHoaDon() {
+    }
+
+    public ClassCTHoaDon(String maHD, String maSP, int soLuong, double donGia, double thanhTien) {
+        this.maHD = maHD;
+        this.maSP = maSP;
+        this.soLuong = soLuong;
+        this.donGia = donGia;
+        this.thanhTien = thanhTien;
+    }
+
+    public String getMaHD() {
+        return maHD;
+    }
+
+    public void setMaHD(String maHD) {
+        this.maHD = maHD;
+    }
+
+    public String getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(String maSP) {
+        this.maSP = maSP;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public double getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
+
+    public double getThanhTien() {
+        return thanhTien;
+    }
+
+    public void setThanhTien(double thanhTien) {
+        this.thanhTien = thanhTien;
+    }
+    
+    
+}
--- src/myClass/ClassHoaDon.java
+++ src/myClass/ClassHoaDon.java
@@ -1,1 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myClass;
 
+import java.io.Serializable;
+
+/**
+ *
+ * @author markhyun
+ */
+public class ClassHoaDon implements Serializable{
+
+    private String maHD ;
+    private String maNV ;
+    private String ngayBan;
+    private String maKH;
+    private Double tongTien;
+
+    public ClassHoaDon() {
+    }
+
+    public ClassHoaDon(String maHD, String maNV, String ngayBan, String maKH, Double tongTien) {
+        this.maHD = maHD;
+        this.maNV = maNV;
+        this.ngayBan = ngayBan;
+        this.maKH = maKH;
+        this.tongTien = tongTien;
+    }
+
+    public String getMaHD() {
+        return maHD;
+    }
+
+    public void setMaHD(String maHD) {
+        this.maHD = maHD;
+    }
+
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getNgayBan() {
+        return ngayBan;
+    }
+
+    public void setNgayBan(String ngayBan) {
+        this.ngayBan = ngayBan;
+    }
+
+    public String getMaKH() {
+        return maKH;
+    }
+
+    public void setMaKH(String maKH) {
+        this.maKH = maKH;
+    }
+
+    public Double getTongTien() {
+        return tongTien;
+    }
+
+    public void setTongTien(Double tongTien) {
+        this.tongTien = tongTien;
+    }
+    
+    
+}
